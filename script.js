b1091a77d269be4d6a3a3b920293c89fa3281376
Sidd Bairan <bairansa@gmail.com>
2021-07-29 14:56:25 +0800

    Create functionality that adds books to the table.

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 let myLibrary = [
     {
-        author:"Goodrich and Tamassia",
-        pages:924,
-        read:"Read",
-        title:"Data Structures and Algorithms in Java",
+        author: "Goodrich and Tamassia",
+        pages: 924,
+        read: "Read",
+        title: "Data Structures and Algorithms in Java",
     },
 
     {
-        author:"Pearl and Rosenbaum",
-        pages:272,
-        read:"Read",
-        title:"Investment Banking: Valuation, Leveraged Buyouts, and Mergers and Acquisitions",
+        author: "Pearl and Rosenbaum",
+        pages: 272,
+        read: "Read",
+        title: "Investment Banking: Valuation, Leveraged Buyouts, and Mergers and Acquisitions",
     },
 
     {
-        author:"Harper Lee",
-        pages:281,
-        read:"Not yet read",
-        title:"To Kill a Mockingbird",
+        author: "Harper Lee",
+        pages: 281,
+        read: "Not yet read",
+        title: "To Kill a Mockingbird",
     },
 ];
 
@@ -31,10 +31,14 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
+    displayBooks();
 }
 
 function displayBooks() {
     const tableBody = document.querySelector('.table-body');
+    /*Emptying the textContent of tableBody avoids the table to 
+    duplicate itself everytime the add book button is pressed.*/
+    tableBody.textContent = '';
     for (let book = 0; book < myLibrary.length; book += 1) {
         const tableRow = document.createElement('tr');
         tableBody.appendChild(tableRow);
@@ -43,9 +47,9 @@ function displayBooks() {
         const editButton = document.createElement('td');
         tableRow.appendChild(editButton);
 
-        function add_img() { 
-            const img = document.createElement('img'); 
-            img.src = 'Images/edit-button-colored.png'; 
+        function add_img() {
+            const img = document.createElement('img');
+            img.src = 'Images/edit-button-colored.png';
             editButton.appendChild(img);
             img.classList.add('edit-button');
         }
@@ -70,6 +74,36 @@ function displayBooks() {
         const bookStatus = document.createElement('td');
         bookStatus.textContent = myLibrary[book].read;
         tableRow.appendChild(bookStatus);
+    }
+}
+
+const addBook = document.querySelector('#add-book');
+const bookTitle = document.querySelector('#title');
+const bookAuthor = document.querySelector('#author');
+const bookPages = document.querySelector('#pages');
+const checkBox = document.querySelector('#checkbox');
+
+addBook.addEventListener('click', addBookInput);
+checkBox.addEventListener('click', changeMessage);
+
+function addBookInput() {
+    function bookStatus() {
+        if (checkBox.checked === true) {
+            return 'Read'
+        } else {
+            return 'Not yet read';
+        }
+    }
+    addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value, bookStatus());
+}
+
+
+function changeMessage() {
+    const readStatusMessage = document.querySelector('#read-status-message');
+    if (checkBox.checked === true) {
+        readStatusMessage.textContent = 'Read';
+    } else {
+        readStatusMessage.textContent = 'Not yet read';
     }
 }
 
