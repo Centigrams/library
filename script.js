372e74b3b5f65140bf612d45719cec7ad004dd4b
Sidd Bairan <bairansa@gmail.com>
2021-08-06 18:58:22 +0800

    Change book constructor into a factory function to suppress warnings.

--- script.js
+++ script.js
@@ -22,14 +22,13 @@ let myLibrary = [
 ];
 
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+    return {
+    title, author, pages, read
+    }
 }
 
 function addBookToLibrary(title, author, pages, read) {
-    let newBook = new Book(title, author, pages, read);
+    let newBook = Book(title, author, pages, read);
     myLibrary.push(newBook);
     displayBooks();
 }
